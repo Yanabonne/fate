bc7d73b69d1cd5ab3f54b55b7feadc26450c43f7
iana <yanamormmm@gmail.com>
2024-08-13 14:19:30 +0500

    feat: add footer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import "../styles/Home/Home.scss";
 import Header from "../shared/Header/Header";
 import Main from "../widgets/Home/Main";
+import Footer from "../shared/Footer/Footer";
 
 export default function Home() {
   return (
     <div className="home">
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
