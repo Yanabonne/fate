87d55787362e7d2632f5ee9af726916fd31c6462
iana <yanamormmm@gmail.com>
2024-08-19 11:58:44 +0500

    feat: update hash routing, form input date and tooltip cycles

--- src/widgets/Cycles/CustomTooltip.tsx
+++ src/widgets/Cycles/CustomTooltip.tsx
@@ -9,13 +9,11 @@ export const CustomTooltip = ({ active, payload, label }: TTooltipProps) => {
     return (
       <div className="cycles__tooltip">
         <span>{label}</span>
-        <br />
         {payload.map((ele: { name: string; value: number }, index: number) => (
           <>
             <small key={index} className="text-secondary">
               {ele.name}: {ele.value}
             </small>
-            <br />
           </>
         ))}
       </div>
